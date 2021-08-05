4c7fd7172671bd33e5280930e992bd360a199c12
Hitarth Bhatt <hitarth.b@simformsolutions.com>
2021-08-05 15:42:52 +0530

    Generate Database Generic Handler For Realm

--- CoreStorage/DatabaseHelper/DatabaseHandler.swift
+++ CoreStorage/DatabaseHelper/DatabaseHandler.swift
@@ -11,31 +11,40 @@ import RealmSwift
 
 class DatabaseHandler {
     
+    static var shared: DatabaseHandler = DatabaseHandler()
     var realm: Realm?
     
     init() {
-        
-        
-        
+        do {
+            self.realm = try Realm()
+        } catch {
+            print("Error in setup Realm ", error.localizedDescription)
+        }
     }
     
     func add<T: Object>(_ type: T.Type) -> T? {
         let object = T()
-        
         return object
     }
     
     
-    func fetch<T: Object>(_ type: T.Type) -> [T] {
-        return []
+    func fetch<T: Object>(_ type: T.Type) -> Results<T>? {
+        let result = realm?.objects(type)
+        return result
     }
     
-    func save() {
-       
+    func save<T: Object>(_ data: T) {
+        do {
+            try self.realm?.write {
+                realm?.add(data)
+            }
+        }catch {
+            print("error in saving data", error.localizedDescription)
+        }
     }
     
     func delete(_ data: Object) {
-        
+        self.realm?.delete(data)
     }
     
     
